14d4099855abff50e10620de1ca9fcebb04abb2e
piller <vilya1997@bk.ru>
2017-08-21 23:47:31 +0300

    add runtime.txt

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import telebot
+import config
 
-token = '436750663:AAHobagg4S1NiG4iWJx1B5diAys2E78a_oE'
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
